eb4b86c7060df904a141fc1eede5070dc26d64f2
yunxiao_liu <yunxiao_liu@cncbinternational.com>
2022-12-29 16:13:25 +0800

    chore: use the same port with both frontend and backend

--- server/main.ts
+++ server/main.ts
@@ -1,19 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createServer as createViteServer } from 'vite';
-
-const setupConfig = {
-  clientPort: 1337,
-  serverPort: 1338
-};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const vite = await createViteServer();
-
-  await app.listen(setupConfig.serverPort);
-
-  await vite.listen(setupConfig.clientPort);
+  app.setGlobalPrefix('api');
+  return app;
 }
-bootstrap();
+
+// 如果是生产模式，直接监听端口
+if (import.meta.env.PROD) {
+  bootstrap().then((app) => {
+    app.listen(3000);
+  });
+}
+
+export const viteNodeApp = bootstrap();
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,49 @@
 import { defineConfig } from 'vite';
 import { join } from 'path';
 import vue from '@vitejs/plugin-vue';
+import { VitePluginNode } from 'vite-plugin-node';
 
 const fileDirToPath = (dir) => join(__dirname, dir);
 
 export default defineConfig(() => {
   return {
-    base: 'https://foo.com/',
     envDir: fileDirToPath('client/config'),
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      ...VitePluginNode({
+        async adapter({ app, req, res, next }) {
+          // 如果url是包含api，则交给nest处理，否则由vite处理
+          if (req.url.indexOf('api') > -1) {
+            await app.init();
+            const instance = app.getHttpAdapter().getInstance();
+            instance(req, res);
+          } else {
+            next();
+          }
+        },
+        appPath: './server/main.ts',
+        exportName: 'viteNodeApp',
+        tsCompiler: 'swc',
+        swcOptions: {}
+      })
+    ],
     publicDir: fileDirToPath('client/public'),
     build: {
       manifest: true,
       rollupOptions: {
         input: fileDirToPath('client/index.html')
       }
+    },
+
+    optimizeDeps: {
+      exclude: [
+        '@nestjs/microservices',
+        '@nestjs/websockets',
+        'cache-manager',
+        'class-transformer',
+        'class-validator',
+        'fastify-swagger'
+      ]
     }
   };
 });
